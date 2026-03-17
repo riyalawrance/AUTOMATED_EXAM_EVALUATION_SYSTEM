4c83fbc67086cb98dfa91366d4f39a7c928b9e26
S DEVI PARVATHY <deviparvathys2005@gmail.com>
2026-03-17 23:06:52 +0530

    Update uploadscriptRoutes.js

--- Backend/routes/uploadscriptRoutes.js
+++ Backend/routes/uploadscriptRoutes.js
@@ -42,7 +42,7 @@ router.post(
           });
         }
 
-        const key = `${course}/${classId}/${examType}/${examId}/answer-scripts/${file.originalname}`;
+        ${course}/${classId}/${examType}/${examId}/answer-scripts/${file.originalname}
 
         const params = {
           Bucket: process.env.S3_BUCKET,
